4ca6988c83c6ca55915d290cb4ca69e1fed75c8e
fmdcba <facudiaz1738@hotmail.com>
2025-01-09 19:38:44 -0300

    add properties for user entity

--- src/main/java/com/mindhub/todolist/models/UserEntity.java
+++ src/main/java/com/mindhub/todolist/models/UserEntity.java
@@ -1,4 +1,53 @@
 package com.mindhub.todolist.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    public UserEntity(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
