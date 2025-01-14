2dadf97f81572a1bad574fe913ca59cd4dce1a8c
fmdcba <facudiaz1738@hotmail.com>
2025-01-14 01:55:28 -0300

    create service to load user details from db

---
+++ src/main/java/com/mindhub/todolist/config/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.mindhub.todolist.config;
+import com.mindhub.todolist.models.UserEntity;
+import com.mindhub.todolist.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return new User(userEntity.getEmail(), userEntity.getPassword(), AuthorityUtils.createAuthorityList(userEntity.getRole().toString()));
+    }
+}
