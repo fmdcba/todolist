5946c626c38a4c2ad79b9ca3118ecbdd2efde037
fmdcba <facudiaz1738@hotmail.com>
2025-01-14 01:57:50 -0300

    create method to find user by username

--- src/main/java/com/mindhub/todolist/repositories/UserRepository.java
+++ src/main/java/com/mindhub/todolist/repositories/UserRepository.java
@@ -3,7 +3,11 @@ package com.mindhub.todolist.repositories;
 import com.mindhub.todolist.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
 
     boolean existsByUsername(String username);
 
