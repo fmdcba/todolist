79bfa9c7835d566fd6eba92b1f11703dcab9eb20
fmdcba <facudiaz1738@hotmail.com>
2025-01-15 03:09:02 -0300

    add admin user for api testing purposes

--- src/main/java/com/mindhub/todolist/TodolistApplication.java
+++ src/main/java/com/mindhub/todolist/TodolistApplication.java
@@ -1,5 +1,6 @@
 package com.mindhub.todolist;
 
+import com.mindhub.todolist.models.RoleType;
 import com.mindhub.todolist.models.Task;
 import com.mindhub.todolist.models.UserEntity;
 import com.mindhub.todolist.repositories.TaskRepository;
@@ -25,12 +26,14 @@ public class TodolistApplication {
 	public CommandLineRunner initData(UserRepository userRepository, TaskRepository taskRepository) {
 
 		return args -> {
-			UserEntity user = new UserEntity("pepe", passwordEncoder.encode("pepe123"), "pepe@pepe.com");
-			UserEntity user1 = new UserEntity("fulano", passwordEncoder.encode("fulano123"), "fulano@fulano.com");
-			UserEntity user2 = new UserEntity("juan", passwordEncoder.encode("juan123"), "juan@juan.com");
+			UserEntity user = new UserEntity("pepe", passwordEncoder.encode("pepe123"), "pepe@pepe.com", RoleType.USER);
+			UserEntity user1 = new UserEntity("fulano", passwordEncoder.encode("fulano123"), "fulano@fulano.com", RoleType.USER);
+			UserEntity user2 = new UserEntity("juan", passwordEncoder.encode("juan123"), "juan@juan.com", RoleType.USER);
+			UserEntity user3 = new UserEntity("admin", passwordEncoder.encode("admin123"), "admin@admin.com", RoleType.ADMIN);
 			userRepository.save(user);
 			userRepository.save(user1);
 			userRepository.save(user2);
+			userRepository.save(user3);
 			Task task1 = new Task("Ir al gym", "Hacer rutina de piernas", Task.TaskStatus.PENDING, user);
 			Task task2 = new Task("Desayunar", "Huevos revueltos", Task.TaskStatus.COMPLETED, user1);
 			taskRepository.save(task1);
