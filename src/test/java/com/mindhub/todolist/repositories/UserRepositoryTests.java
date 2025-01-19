fa7bd8f2b6fccabd6b63af6e7a816e3193efbd4f
fmdcba <facudiaz1738@hotmail.com>
2025-01-19 18:40:50 -0300

    ignore file for testing and create first test

---
+++ src/test/java/com/mindhub/todolist/repositories/UserRepositoryTests.java
@@ -0,0 +1,12 @@
+package com.mindhub.todolist.repositories;
+
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class UserRepositoryTests {
+
+
+}
